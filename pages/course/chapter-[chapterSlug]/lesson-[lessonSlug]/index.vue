716d5c6cdf554a30f7ea7e48c32ef663cfd2b070
gumrahsindar <gsindar@gmail.com>
2024-12-23 16:56:43 +0300

    refactor course page layout and add lesson detail page

---
+++ pages/course/chapter-[chapterSlug]/lesson-[lessonSlug]/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useCourse } from '~/composables/useCourse'
+
+const course = useCourse()
+const route = useRoute()
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  )
+})
+
+const lesson = computed(() => {
+  return chapter.value?.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  )
+})
+
+console.log(chapter)
+</script>
+
+<template>
+  <div>
+    <h1>Chapter</h1>
+    <h2>Lesson</h2>
+    <p v-if="chapter">
+      {{ chapter.title }}
+    </p>
+    <p v-if="lesson">{{ lesson.title }}</p>
+  </div>
+</template>
+
+<style scoped></style>
