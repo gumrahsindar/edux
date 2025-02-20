d6b33ca390eaa6d2b85a26f22aa88e1d08e00e7f
gumrahsindar <gsindar@gmail.com>
2025-02-20 17:37:17 +0300

    update authentication middleware to allow access for logged-in users and adjust login redirection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false,
+  },
 })
