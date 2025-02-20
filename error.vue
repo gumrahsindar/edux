52b9ef9a848d4baf94cca5be36cf5b086d00b5bf
gumrahsindar <gsindar@gmail.com>
2025-02-20 22:49:24 +0300

    fix: update error handling redirect path format

--- error.vue
+++ error.vue
@@ -4,7 +4,7 @@ const error = useError()
 const handleError = () => {
   clearError({
     redirect:
-      '/course/chapter-1-chapter-1/lesson-1-introduction-to-typescript-with-vue-js-3',
+      '/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3',
   })
 }
 </script>
