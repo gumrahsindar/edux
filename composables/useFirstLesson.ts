96027f25d89bdbb40e3f5b2d834990d99edd9bff
gumrahsindar <gsindar@gmail.com>
2025-02-21 22:55:57 +0300

    refactor: replace course data structure with API call for course metadata and first lesson retrieval

--- composables/useFirstLesson.ts
+++ composables/useFirstLesson.ts
@@ -1,4 +1,5 @@
-export default () => {
-  const { chapters } = useCourse()
-  return chapters[0].lessons[0]
+export default async () => {
+  const course = await useCourse()
+  return (course.value as { chapters: { lessons: any[] }[] }).chapters[0]
+    .lessons[0]
 }
