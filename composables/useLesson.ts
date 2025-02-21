3c39b6281f42f86156c4adfa69e10c1656e0dcf7
gumrahsindar <gsindar@gmail.com>
2025-02-21 21:51:36 +0300

    add: implement caching mechanism for lesson data fetching

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,22 +1,2 @@
-export default async (chapterSlug: string, lessonSlug: string) => {
-  const nuxtApp = useNuxtApp()
-  console.log('nuxtApp', nuxtApp)
-  const { data, error } = await useFetch(
-    `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`,
-    {
-      getCachedData(key) {
-        return nuxtApp.payload.data[key] || nuxtApp.static.data[key]
-      },
-    }
-  )
-
-  if (error.value) {
-    throw createError({
-      ...error.value,
-
-      statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-    })
-  }
-
-  return data
-}
+export default async (chapterSlug: string, lessonSlug: string) =>
+  useFetchWithCache(`/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`)
