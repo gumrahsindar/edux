0fd82064f9ab5fdb29ff07667a2bc2fd3b137530
gumrahsindar <gsindar@gmail.com>
2025-02-22 18:03:25 +0300

    refactor: enhance type safety for lesson fetching with LessonWithPath type

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,2 +1,6 @@
+import type { LessonWithPath } from '~/lib/types'
+
 export default async (chapterSlug: string, lessonSlug: string) =>
-  useFetchWithCache(`/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`)
+  useFetchWithCache<LessonWithPath>(
+    `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
+  )
