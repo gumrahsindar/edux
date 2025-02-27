cdf9a76dd9ec425115edbc85902d394f41a9ec30
gumrahsindar <gsindar@gmail.com>
2025-02-27 20:33:39 +0300

    refactor: remove hardcoded course data type and retain LessonWithPath definition

--- lib/types.ts
+++ lib/types.ts
@@ -3,21 +3,3 @@ import type { Lesson } from '@prisma/client'
 export type LessonWithPath = Lesson & {
   path: string
 }
-
-export type TCourseData = {
-  title: string
-  chapters: {
-    title: string
-    slug: string
-    number: number
-    lessons: {
-      title: string
-      slug: string
-      number: number
-      downloadUrl: string
-      sourceUrl?: string
-      videoId: number
-      text: string
-    }[]
-  }[]
-}
